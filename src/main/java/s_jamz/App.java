3ba6b8bbbdaccaaf3e1a26316effda71a4c1d882
Enadiz76 <zidtimo@gmail.com>
2024-10-30 22:10:36 -0400

    Takes user input and Extracts to hard coded directory

--- src/main/java/s_jamz/App.java
+++ src/main/java/s_jamz/App.java
@@ -1,6 +1,7 @@
 package s_jamz;
 
 import java.io.File;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -10,24 +11,19 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("This is a test");
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Please input the directory path of the zip file");
-        FileExtractor fileExtractor = new FileExtractor();
-        
-        //read in input from user for zip file path
-        java.util.Scanner scanner = new java.util.Scanner(System.in);
-        // String zipFilePath = scanner.nextLine();
-        File zipFilePath = new File("D:\\UWI\\Year 3\\Sem 1\\Comp3607\\Project\\Comp3607_Project\\src\\resources\\Maia_Neptune_816030520_A1.zip");
-
-        //read in input from user for directory path to extract the zip file to
-        System.out.println("Please input the directory path to extract the zip file to");
-        // String destDir = scanner.nextLine(); //need to get a better way to input the directory path
-        File destDir = new File("D:\\UWI\\Year 3\\Sem 1\\Comp3607\\Project\\Comp3607_Project\\src\\resources\\");
-
-        //unzip the file
-        fileExtractor.extractZip(zipFilePath, destDir);
-
+        String zipFilePath = scanner.nextLine();
         scanner.close();
+
+
+        FileExtractor fileExtractor = new FileExtractor();
+        File zipFile = new File(zipFilePath);
+        try{
+            fileExtractor.extractZip(zipFile);
+        }
+        catch(Exception e){
+            e.printStackTrace();   
+        }
     }
 }
