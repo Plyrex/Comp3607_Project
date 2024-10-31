94b876759c40e3058da3260842956d10fc246c0a
Enadiz76 <zidtimo@gmail.com>
2024-10-30 22:45:13 -0400

    Working dynamic Extraction of Student Files

--- src/main/java/s_jamz/FileExtractor.java
+++ src/main/java/s_jamz/FileExtractor.java
@@ -1,55 +1,21 @@
 package s_jamz;
 
-// import java.io.File;
-// import java.io.FileInputStream;
-// import java.io.FileOutputStream;
-// import java.io.IOException;
-// import java.util.zip.ZipEntry;
-// import java.util.zip.ZipInputStream;
-// import net.lingala.zip4j.exception.ZipException;
-// import net.lingala.zip4j.core.ZipFile;
-
+import net.lingala.zip4j.exception.ZipException;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+import net.lingala.zip4j.ZipFile;
 
 
 public class FileExtractor {
-    public void extractZip(File zipFile) {
-        File dir = new File(zipFile.getParent() + File.separator + zipFile.getName().substring(0, zipFile.getName().lastIndexOf(".")));
-        // create output directory if it doesn't exist
-        if(!dir.exists()) dir.mkdirs();
-        FileInputStream fis;
-        //buffer for read and write data to file
-        byte[] buffer = new byte[1024];
+    public void extractZip(File zipFile) throws IOException {
+        String destFolder = System.getProperty("user.dir") + "\\src\\main\\java\\s_jamz\\resources\\Submissions";
         try {
-            fis = new FileInputStream(zipFile);
-            ZipInputStream zis = new ZipInputStream(fis);
-            ZipEntry ze = zis.getNextEntry();
-            while(ze != null){
-                String fileName = ze.getName();
-                File newFile = new File(dir + File.separator + fileName);
-                System.out.println("Unzipping to "+newFile.getAbsolutePath());
-                //create directories for sub directories in zip
-                new File(newFile.getParent()).mkdirs();
-                FileOutputStream fos = new FileOutputStream(newFile);
-                int len;
-                while ((len = zis.read(buffer)) > 0) {
-                fos.write(buffer, 0, len);
-                }
-                fos.close();
-                //close this ZipEntry
-                zis.closeEntry();
-                ze = zis.getNextEntry();
-            }
-            //close last ZipEntry
-            zis.closeEntry();
-            zis.close();
-            fis.close();
-        } catch (IOException e) {
+            ZipFile zip = new ZipFile(zipFile);
+            zip.extractAll(destFolder);          
+            System.out.println("File unzipped successfully\n");
+        } 
+        catch (ZipException e) {
+            System.out.println("Error unzipping file\n");
             e.printStackTrace();
         }
     }
