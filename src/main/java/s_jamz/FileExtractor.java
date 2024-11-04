c8d52c1a0bac591a76189bb46abd63e8b6c58a1f
Enadiz76 <zidtimo@gmail.com>
2024-11-04 14:43:40 -0400

    Fixed Folder Pathing???

--- src/main/java/s_jamz/FileExtractor.java
+++ src/main/java/s_jamz/FileExtractor.java
@@ -7,8 +7,8 @@ import net.lingala.zip4j.exception.ZipException;
 
 public class FileExtractor {
     public void extractZip(File zipFile) throws IOException {
-        String destFolder = System.getProperty("user.dir") + "\\src\\main\\java\\resources\\Submissions";
-        String newDestFolder = System.getProperty("user.dir") + "\\src\\main\\java\\resources\\StudentFolders";
+        String destFolder = System.getProperty("user.dir") + "\\src\\main\\resources\\Submissions";
+        String newDestFolder = System.getProperty("user.dir") + "\\src\\main\\resources\\StudentFolders";
         
         // Create Submissions directory if it doesn't exist
         File submissionsDir = new File(destFolder);
