9c9a2438534715b84da0fc6a48e02ef48c9b24ec
jonsnation <jonathanswamber2003@gmail.com>
2024-11-09 13:35:33 -0400

    removed perms

--- src/main/java/s_jamz/StrategyPattern/EvaluationStrategy.java
+++ src/main/java/s_jamz/StrategyPattern/EvaluationStrategy.java
@@ -3,5 +3,6 @@ package s_jamz.StrategyPattern;
 import java.io.File;
 
 public interface EvaluationStrategy {
-    public void evaluate(File javaFile);
+    void evaluate(File javaFile);
+    void runTests(File javaFile);
 }
---
+++ src/main/java/s_jamz/StrategyPattern/TestContext.java
@@ -0,0 +1,27 @@
+package s_jamz.StrategyPattern;
+
+import java.io.File;
+
+public class TestContext {
+    private EvaluationStrategy strategy;
+
+    public void setStrategy(EvaluationStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void evaluate(File javaFile) {
+        if (strategy != null) {
+            strategy.evaluate(javaFile);
+        } else {
+            throw new IllegalStateException("Evaluation strategy not set");
+        }
+    }
+
+    public void runTests(File javaFile) {
+        if (strategy != null) {
+            strategy.runTests(javaFile);
+        } else {
+            throw new IllegalStateException("Test strategy not set");
+        }
+    }
+}
