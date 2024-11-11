b10203a00f92ca3bdd683aab1ccfeac80c2e552e
jonsnation <jonathanswamber2003@gmail.com>
2024-11-10 22:39:23 -0400

    fixed an issue

--- src/main/java/s_jamz/TemplatePattern/FileProcessorTemplate.java
+++ src/main/java/s_jamz/TemplatePattern/FileProcessorTemplate.java
@@ -1,17 +1,15 @@
 package s_jamz.TemplatePattern;
 
-import javax.tools.DiagnosticCollector;
-import javax.tools.JavaFileObject;
 import java.io.File;
 
 public abstract class FileProcessorTemplate {
     protected abstract void extractFile(File zipFile);
     protected abstract void compileFile(File file);
-    protected abstract void storeResults(File file, DiagnosticCollector<JavaFileObject> diagnostics);
-    
+    protected abstract void storeResults(File file);
+
     public void processFile(File file) {
         extractFile(file);
-        compileFile(file);
+        // Compilation will be handled separately
     }
 
     public void compileDirectory(File directory) {
