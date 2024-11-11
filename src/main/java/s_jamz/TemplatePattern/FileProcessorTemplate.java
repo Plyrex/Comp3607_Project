2f2746f8dbbe909fa3beb16e86aa2d94edae2a18
jonsnation <jonathanswamber2003@gmail.com>
2024-11-10 23:13:53 -0400

    minor path change

--- src/main/java/s_jamz/TemplatePattern/FileProcessorTemplate.java
+++ src/main/java/s_jamz/TemplatePattern/FileProcessorTemplate.java
@@ -1,14 +1,17 @@
 package s_jamz.TemplatePattern;
 
 import java.io.File;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaFileObject;
 
 public abstract class FileProcessorTemplate {
     protected abstract void extractFile(File zipFile);
     protected abstract void compileFile(File file);
-    protected abstract void storeResults(File file);
+    protected abstract void storeResults(File file, DiagnosticCollector<JavaFileObject> diagnostics);
 
     public void processFile(File file) {
         extractFile(file);
+        // Compilation will be handled separately
     }
 
     public void compileDirectory(File directory) {
