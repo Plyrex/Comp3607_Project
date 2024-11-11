0e985834d515b36c9528cc4da6829b35d7ea3b1c
jonsnation <jonathanswamber2003@gmail.com>
2024-11-11 18:17:11 -0400

    do we need this?

--- src/main/java/s_jamz/TemplatePattern/FileProcessorTemplate.java
+++ src/main/java/s_jamz/TemplatePattern/FileProcessorTemplate.java
@@ -7,7 +7,7 @@ import javax.tools.JavaFileObject;
 public abstract class FileProcessorTemplate {
     protected abstract void extractFile(File zipFile);
     protected abstract void compileFile(File file);
-    protected abstract void storeResults(File file, DiagnosticCollector<JavaFileObject> diagnostics);
+    protected abstract void storeResults(File file, DiagnosticCollector<JavaFileObject> diagnostics, String compilerOutput);
 
     public void processFile(File file) {
         extractFile(file);
