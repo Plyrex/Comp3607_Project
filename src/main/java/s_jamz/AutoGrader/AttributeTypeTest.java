e607bedb9a219cf531653a61cc172a4811a3850f
Maia Neptune <maianeptune@gmail.com>
2024-11-14 13:51:33 -0400

    not sure what i changed

--- src/main/java/s_jamz/AutoGrader/AttributeTypeTest.java
+++ src/main/java/s_jamz/AutoGrader/AttributeTypeTest.java
@@ -31,13 +31,14 @@ public class AttributeTypeTest {
     }
 
     @BeforeEach
-    public void setup() {
-        try {
-            loadAttributeNames("ChatBot");
-            loadAttributeNames("ChatBotPlatform");
-            loadAttributeNames("ChatBotSimulation");
-        } catch (Exception e) {
-            System.err.println("Could not load attribute names for classes: " + e.getMessage());
+    public void setup(){
+        try{
+        loadAttributeNames("ChatBot");
+        loadAttributeNames("ChatBotPlatform");
+        loadAttributeNames("ChatBotSimulation");
+        }
+        catch(Exception e){
+            System.err.println("Could not load attribute names for classes" + e.getMessage());
         }
     }
 
